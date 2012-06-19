c1d369c5a56c0bc6921cdf20b2bd79e1c472cd5b
Andrei Oprea <andrei.br92@gmail.com>
2012-06-19 10:44:54 +0300

    added requirejs/refactoring code

--- js/script.js
+++ js/script.js
@@ -1,259 +1,280 @@
-//"use strict"
-
-var Kreator = (function(options) {
-
-	var slideX = 0,
-		slideY = 0,
-		$ = options.$,
-		dummyText = $('<span>new slide</span>'),
-		Reveal = options.Reveal,
-		count = 1;
-
-	var addContentToSlide = function() {
-		$(this).html('');
-		console.log('adding content');
-		console.log($(this));
-		if($(this).parent().hasClass('slides')) { // if it's a section with no children
-			var $span = $('<span/>')
-					.on('click', Kreator.editSection);
-			$span.appendTo($(this)).trigger('click'); // append to section and trigger edit
-			$('.content').focus();
-		}
-	};
+define(['text', 'jquery'], function(textStyle, $){
 	
-	Reveal.addEventListener( 'slidechanged', function( event ) {
-		hideFooter();
-		Kreator.setSlideX(event.indexh);
-		Kreator.setSlideY(event.indexv);
-	});
+	var Kreator = (function (options) {
 
-	options.down.on('click', function(){
-		Kreator.addSlideDown('dummy');
-		Reveal.navigateDown();
-	});
+		var slideX = 0,
+			slideY = 0,
+			$ = options.jquery,
+			Reveal = options.reveal,
+			dummyText = $('<span>new slide</span>'),
+			$span;
 
-	options.right.on('click', function(){
-		Kreator.addSlideRight('dummy');
-		Reveal.navigateRight();
-	});
+		var init = function() {
 
-	var setSlideX = function(x) {
-		slideX = x;
-	};
+			options.right = $('<div data-direction="right"></div>')
+					.addClass('add-slide add-right')
+					.text('+');
+			
+			options.down = $('<div data-direction="bottom"></div>')
+					.addClass('add-slide add-down')
+					.text('+');
 
-	var setSlideY = function(y) {
-		slideY = y;
-	};
+			$('body').append(options.right)
+				.append(options.down);
 
-	var getCurrentSlide = function() {
-		var s = $('.present');
-		if(!s.hasClass('stack')) {
-			if(s.length > 1) {
-				return s.eq(slideX);
-			} else {
-				return s;
+			options.down.on('click', function(){
+				Kreator.addSlideDown('dummy');
+				Reveal.navigateDown();
+			});
+
+			options.right.on('click', function(){
+				Kreator.addSlideRight('dummy');
+				Reveal.navigateRight();
+			});
+
+			Reveal.addEventListener( 'slidechanged', function( event ) {
+				Kreator.hideFooter();
+				Kreator.setSlideX(event.indexh);
+				Kreator.setSlideY(event.indexv);
+			});
+
+			$('section').on('click', function(){
+				var s = Kreator.getCurrentSlide();
+				var span = $('<span/>').on('click', Kreator.editSection);
+				s.append(span);
+				span.trigger('click');
+			});
+
+			$('section>*').on('click', Kreator.editSection);
+
+			$('#footer .content').on('keyup', function(e){
+				var string = $('.present').text();
+				
+				if(e.keyCode == 13) {
+					this.innerHTML = this.innerHTML.replace(/<div>/gi, '')
+									.replace(/<\/div>/gi, '')
+									.replace(/&nbsp;/gi, ' <br>');
+				}
+
+				$('#word-count').text(string.split(' ').length);
+				$('#char-count').text(string.length);
+				$span.html(this.innerHTML);
+			});
+
+			$('.btn').on('click', function(){
+				var tag = $(this).data('textstyle');
+				if(tag === 'a')
+					textStyle.insertHiperlink(this, $span);	
+				else {
+					var string = textStyle.format(this);
+					$('.content').html(string);
+					updateSectionContent();
+				}
+			});
+
+		};
+
+		var addContentToSlide = function() {
+			var s = this.getCurrentSlide();
+			var span = $('<span/>').on('click', this.editSection);
+			s.append(span);
+			span.trigger('click');
+		};
+		
+
+		var setSlideX = function(x) {
+			slideX = x;
+		};
+
+		var setSlideY = function(y) {
+			slideY = y;
+		};
+
+		var getCurrentSlide = function() {
+			var s = $('.present');
+			if(!s.hasClass('stack')) {
+				if(s.length > 1) {
+					return s.eq(slideX);
+				} else {
+					return s;
+				}
 			}
-		}
-		else {
-			return $('section', s).eq(slideY);
-		}
-	};
+			else {
+				return $('section', s).eq(slideY);
+			}
+		};
 
-	var getDownSlide = function() {
-		var s = $('.slides>section').eq(slideX),
-			c = $('section', s);
-		if(!c.length) return 0;
-		else {
-			return c[this.slideY+1];
-		}
-	};
+		var getDownSlide = function() {
+			var s = $('.slides>section').eq(slideX),
+				c = $('section', s);
+			if(!c.length) return 0;
+			else {
+				return c[this.slideY+1];
+			}
+		};
 
-	var addSlideRight = function(content) {
-		var c = dummyText.clone().html('<h2>' + (++count) + '</h2>').on('click', editSection),
-			s = this.getCurrentSlide();
+		var addSlideRight = function(content) {
+			var c = dummyText.clone().on('click', this.editSection),
+				s = this.getCurrentSlide();
 
-		// if the current slide is the last slide on the X axis we append to the parent
-		if($('.slides>section').length == slideX+1) {
-			var section = $('<section/>').append(c);
-			$('.slides').append(section);
-		} else {
-			// else we just append after the current element
+			// if the current slide is the last slide on the X axis we append to the parent
+			if($('.slides>section').length == slideX+1) {
+				var section = $('<section/>').append(c);
+				$('.slides').append(section);
+			} else {
+				// else we just append after the current element
+				$('<section/>')
+					.append(c)
+					.insertAfter(s);
+			}
+		};
+
+		var addSlideLeft = function(content) {
+			dummyText.on('click', this.editSection);
+			var s = this.getCurrentSlide();
 			$('<section/>')
-				.append(c)
-				.insertAfter(s);
-		}
-	};
+				.append(dummyText.clone())
+				.insertBefore(s);
+		};
 
-	var addSlideLeft = function(content) {
-		dummyText.on('click', editSection);
-		var s = this.getCurrentSlide();
-		$('<section/>')
-			.append(dummyText.clone())
-			.insertBefore(s);
-	};
+		var addSlideDown = function(content) {
 
-	var addSlideDown = function(content) {
+			var s = this.getCurrentSlide();
+			var d = dummyText.clone().on('click', this.editSection);
 
-		var s = this.getCurrentSlide();
-		dummyText.on('click', editSection);
+			if(s.parent().hasClass('slides')) {
+				var c = $('<section/>').append(s.html());
+				var ns = $('<section/>').append(d);
 
-		if(s.parent().hasClass('slides')) {
-			var c = $('<section/>').append(s.html());
-			var ns = $('<section/>').append(dummyText.clone());
+				s.html('').append(c).append(ns);
+			} else {
+				$('<section/>').append(d).insertAfter(s);
+			}
 
-			s.html('').append(c).append(ns);
-		} else {
-			$('<section/>').text('new slide').insertAfter(s);
-		}
+		};
 
-	};
+		var editSection = function(e){
+			e.stopPropagation();
+			var tagN = this.nodeName.toLowerCase()
+				, content = '';
 
-	var editSection = function(e){
-		e.stopPropagation();
-		console.log('edit section', $(this));
-		//if($('section', $(this).parent()).length) return; // weird bug this needs fixing !
-		if(!$(this).parent().hasClass('present')) return;
-		$editable = $(this);
+			$span = $(this);
 
-		var tagN = this.nodeName.toLowerCase(),
-			content = $editable.html();
-
-		if(['div', 'img'].indexOf(tagN) >= 0) return;
-
-		if(content[0] !== '<' && tagN !== 'span')
-			$('#footer').css({ 'bottom' : 0 })
+			if(tagN !== 'span') content = '<' + tagN + '>' + $(this).html() + '</' + tagN + '>';
+			else content = $(this).html();
+			$('#footer').css({'bottom':0})
 				.children('.content')
-				.html('<' + tagN + '>' + content + '</' + tagN + '>');
-			else
-				$('#footer').css({ 'bottom' : 0 })
-					.children('.content')
-					.html(content);
-	};
+				.html(content);
+		};
 
-	return {
-		addSlideDown: addSlideDown,
-		addSlideRight: addSlideRight,
-		addSlideLeft: addSlideLeft,
-		editSection: editSection,
-		setSlideX: setSlideX,
-		setSlideY: setSlideY,
-		getCurrentSlide: getCurrentSlide
-	};
-}({
-	Reveal: Reveal,
-	down: $('.add-down'),
-	right: $('.add-right'),
-	$: $
-}));
+		var updateSectionContent = function() {
+			$span.html($('.content').html());
+		};
 
-var $editable;
+		var hideFooter = function(e){
+			$('#footer').css({
+				'height' : 210,
+				'bottom' : -220
+			}).removeClass();
+		};
 
-var hideFooter = function(e){
-	$('#footer').css({
-		'height' : 210,
-		'bottom' : -220
-	}).removeClass();
-};
+		return {
+			addSlideDown: addSlideDown,
+			addSlideRight: addSlideRight,
+			addSlideLeft: addSlideLeft,
+			editSection: editSection,
+			setSlideX: setSlideX,
+			setSlideY: setSlideY,
+			getCurrentSlide: getCurrentSlide,
+			hideFooter: hideFooter,
+			init: init
+		};
+	})({
+		jquery: $,
+		reveal: Reveal
+	});
 
-$('#footer .content').on('keyup', function(e){
-	var string = $('.present').text();
-	
-	if(e.keyCode == 13) {
-		this.innerHTML = this.innerHTML.replace(/(<div>)?|(<div>)+/gi, '')
-						.replace(/<\/div>/gi, '')
-						.replace(/&nbsp;/gi, ' <br>');
-	}
-
-	$('#word-count').text(string.split(' ').length);
-	$('#char-count').text(string.length);
-	$editable.html(this.innerHTML);
+	return Kreator;
 });
 
-var moveImgs = function(e){
-	if(e.which > 2) return;
-	var o = {
-		x : e.clientX,
-		y : e.clientY,
-		which : e.which
-	};
-	var $that = $(this);
-	if(e.which == 1) {
-		var left = parseInt($that.css('left'), 10) || 0;
-		var top = parseInt($that.css('top'), 10) || 0;
-		$(window).on('mousemove', function(e){
+// var moveImgs = function(e){
+// 	if(e.which > 2) return;
+// 	var o = {
+// 		x : e.clientX,
+// 		y : e.clientY,
+// 		which : e.which
+// 	};
+// 	var $that = $(this);
+// 	if(e.which == 1) {
+// 		var left = parseInt($that.css('left'), 10) || 0;
+// 		var top = parseInt($that.css('top'), 10) || 0;
+// 		$(window).on('mousemove', function(e){
 
-			var x = left + e.clientX - o.x;
-			var y = top + e.clientY - o.y;
-			$that.css({
-				'left' : x,
-				'top' : y
-			});
+// 			var x = left + e.clientX - o.x;
+// 			var y = top + e.clientY - o.y;
+// 			$that.css({
+// 				'left' : x,
+// 				'top' : y
+// 			});
 
-		});
-	} else {
-		var w = parseInt($that.css('width'), 10);
-		var h = parseInt($that.css('height'), 10);
-		$(window).on('mousemove', function(e){
-			var n = {
-				x : e.clientX,
-				y : e.clientY
-			};
-			var d = parseInt(Math.sqrt( (o.x - n.x)*(o.x - n.x) + (o.y - n.y)*(o.y - n.y) ), 10);
-			if(n.x > o.x || n.y > o.y) d = -d;
-			$that.css({
-				'width' : w-d
-			});
-		});
-	}
-	$(window).on('mouseup', function(){
-		$(window).off('mousemove');
-	});
-};
+// 		});
+// 	} else {
+// 		var w = parseInt($that.css('width'), 10);
+// 		var h = parseInt($that.css('height'), 10);
+// 		$(window).on('mousemove', function(e){
+// 			var n = {
+// 				x : e.clientX,
+// 				y : e.clientY
+// 			};
+// 			var d = parseInt(Math.sqrt( (o.x - n.x)*(o.x - n.x) + (o.y - n.y)*(o.y - n.y) ), 10);
+// 			if(n.x > o.x || n.y > o.y) d = -d;
+// 			$that.css({
+// 				'width' : w-d
+// 			});
+// 		});
+// 	}
+// 	$(window).on('mouseup', function(){
+// 		$(window).off('mousemove');
+// 	});
+// };
 
-$('section').on('click', function(){
-	var s = Kreator.getCurrentSlide();
-	var span = $('<span/>').on('click', Kreator.editSection);
-	s.append(span);
-	span.trigger('click');
-})
-
-$('section>*').on('click', Kreator.editSection);
+// $('section>*').on('click', Kreator.editSection);
 
 
-$('.resize').on('mousedown', function(e){
+// $('.resize').on('mousedown', function(e){
 
-	var resizeFooter = function(e) {
-		var nh = $(document).height() - e.clientY;
-		$('#footer').css({ 'height' : nh });
-	};
+// 	var resizeFooter = function(e) {
+// 		var nh = $(document).height() - e.clientY;
+// 		$('#footer').css({ 'height' : nh });
+// 	};
 
-	$(window).on('mousemove', function(e){
-		resizeFooter(e);
-	});
+// 	$(window).on('mousemove', function(e){
+// 		resizeFooter(e);
+// 	});
 
-	$(window).on('mouseup', function(){
-		$(window).off('mousemove');
-	});
-});
+// 	$(window).on('mouseup', function(){
+// 		$(window).off('mousemove');
+// 	});
+// });
 
 
-document.getElementsByTagName('section')[0].ondrop = function(e) {
+// document.getElementsByTagName('section')[0].ondrop = function(e) {
 	
-	e.preventDefault();
+// 	e.preventDefault();
 	
-	var $this = $(this)
-	, file = e.dataTransfer.files[0]
-	, reader = new FileReader();
+// 	var $this = $(this)
+// 	, file = e.dataTransfer.files[0]
+// 	, reader = new FileReader();
 	
-	reader.onload = function (e) {
-		var img = $('<img/>')
-			.attr('src', e.target.result)
-			.on('mousedown', moveImgs)
-			.appendTo($this)
-			.bind('dragstart', function(e) { e.preventDefault(); });
-	};
+// 	reader.onload = function (e) {
+// 		var img = $('<img/>')
+// 			.attr('src', e.target.result)
+// 			.on('mousedown', moveImgs)
+// 			.appendTo($this)
+// 			.bind('dragstart', function(e) { e.preventDefault(); });
+// 	};
 
-	reader.readAsDataURL(file);
-}
+// 	reader.readAsDataURL(file);
+// }
 
-document.getElementsByClassName('close')[0].addEventListener('click', hideFooter, false);
+// document.getElementsByClassName('close')[0].addEventListener('click', hideFooter, false);
--- js/text.js
+++ js/text.js
@@ -1,49 +1,48 @@
-$('button.btn').on('click', function(){ // makes words in bold/italic/underline
-	var s = window.getSelection()
-	, string = $('.content').text()
-	, newstring = ''
-	, tag = $(this).data('textstyle')
-	, find = string.substring(s.baseOffset, s.extentOffset);
+define(['jquery'], function($) {
+	return {
+		format: function(that) {
+			// makes words in bold/italic/underline
+			var s = window.getSelection()
+			, string = $('.content').text()
+			, newstring = ''
+			, tag = $(that).data('textstyle')
+			, find = string.substring(s.baseOffset, s.extentOffset);
 
-	if(s.baseOffset == s.extentOffset) return;
+			if(s.baseOffset == s.extentOffset) return;
 
-	if(tag === 'a') {
-		insertHiperlink($(this));
-		return;
-	}
+			newstring += '<'+ tag +'>';
+			newstring += string.substring(s.baseOffset, s.extentOffset);
+			newstring += '</'+ tag +'>';
+			
+			string = $('.content').html();
+			string = string.replace(find, newstring);
 
-	newstring += '<'+ tag +'>';
-	newstring += string.substring(s.baseOffset, s.extentOffset);
-	newstring += '</'+ tag +'>';
-	
-	string = $('.content').html();
-	string = string.replace(find, newstring);
+			return string;
+		},
+		insertHiperlink: function(that, span) {
+			console.log('inserting link', $(that));
+			var s = window.getSelection()
+				, string = $('.content').text()
+				, newstring = ''
+				, find = string.substring(s.baseOffset, s.extentOffset);
 
-	$('.content').html(string);
-	$editable.html(string);
-});
+			if(!$('input', that).length)
+				$('<input>').appendTo(that)
+					.on('keyup', function(e){
+					var code = (e.keyCode ? e.keyCode : e.which);
+					if(code == 13) {
+						newstring += '<a href="http://'+$(this).val()+'">';
+						newstring += find;
+						newstring += '</a>';
 
-var insertHiperlink = function(that) {
+						string = $('.content').html().replace(find, newstring);
+
+						$(this).remove();
+						$('.content').html(string);
+						span.html(string);
+					}
+				});
+		}
+	};
 		
-	var s = window.getSelection()
-		, string = $('.content').text()
-		, newstring = ''
-		, find = string.substring(s.baseOffset, s.extentOffset);
-
-	if(!$('input', that).length)
-		$('<input>').appendTo(that)
-			.on('keyup', function(e){
-			var code = (e.keyCode ? e.keyCode : e.which);
-			if(code == 13) {
-				newstring += '<a href="http://'+$(this).val()+'">';
-				newstring += find;
-				newstring += '</a>';
-
-				string = $('.content').html().replace(find, newstring);
-
-				$('.content').html(string);
-				$editable.html(string);
-				$(this).remove();
-			}
-		});
-};
+});
